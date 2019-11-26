fea46ff1321d0c6226e2249cacf9026182cfc0ef
Elise Dennis <elisedennisdev@gmail.com>
2019-11-26 15:45:08 +0000

    -Replaced player sprites. -Added animations and animation states for the player. -Created a functioning, working blend tree for the player's animations. -Added to the scene, polishing areas to look realistic and fixing missing textures. -Created a separate tilemap for platforms -Attached a separate collider for platforms and a 2D Effector.

--- 2D Platformer Example Project/2D Platformer Example Project/Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ 2D Platformer Example Project/2D Platformer Example Project/Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -45,7 +45,7 @@ namespace UnityStandardAssets._2D
             m_Anim.SetBool("Ground", m_Grounded);
 
             // Set the vertical animation
-            m_Anim.SetFloat("vSpeed", m_Rigidbody2D.velocity.y);
+            //m_Anim.SetFloat("PSpeed", m_Rigidbody2D.velocity.y);
         }
 
 
@@ -61,6 +61,11 @@ namespace UnityStandardAssets._2D
                 }
             }
 
+            if (Input.GetKeyUp(KeyCode.LeftControl))
+            {
+                crouch = false;
+            }
+
             // Set whether or not the character is crouching in the animator
             m_Anim.SetBool("Crouch", crouch);
 
@@ -71,7 +76,7 @@ namespace UnityStandardAssets._2D
                 move = (crouch ? move*m_CrouchSpeed : move);
 
                 // The Speed animator parameter is set to the absolute value of the horizontal input.
-                m_Anim.SetFloat("Speed", Mathf.Abs(move));
+                m_Anim.SetFloat("PSpeed", Mathf.Abs(move));
 
                 // Move the character
                 m_Rigidbody2D.velocity = new Vector2(move*m_MaxSpeed, m_Rigidbody2D.velocity.y);
