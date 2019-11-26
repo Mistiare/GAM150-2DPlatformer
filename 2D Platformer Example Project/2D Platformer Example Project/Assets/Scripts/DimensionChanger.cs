30ed5d9eae40ca094844c3ee1de71b7d9de71c76
Elise Dennis <elisedennisdev@gmail.com>
2019-11-26 17:21:04 +0000

    -Further refined level design -Began working on DimensionChanger.cs -Created Tilemap_PlatformsNight

---
+++ 2D Platformer Example Project/2D Platformer Example Project/Assets/Scripts/DimensionChanger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DimensionChanger : MonoBehaviour
+{
+
+    public GameObject tilemap;
+    public GameObject tilemap2;
+    private bool active = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && active)
+        {
+            tilemap.SetActive(false);
+            tilemap2.SetActive(true);
+            Debug.Log("disabled");
+            active = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.F) && !active)
+        {
+            tilemap.SetActive(true);
+            tilemap2.SetActive(false);
+            Debug.Log("Enabled");
+            active = true;
+        }
+    }
+}
