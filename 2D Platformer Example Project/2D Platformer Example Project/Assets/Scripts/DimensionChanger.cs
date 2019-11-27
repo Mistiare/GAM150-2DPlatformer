30dc591213dcfe22ebe8a3043f0051d3ce167126
Elise Dennis <elisedennisdev@gmail.com>
2019-11-27 21:51:54 +0000

    -Added Sign.cs -Added SignDialog.png -Implemented mechanical introduction earlier in the level to teach the player basics. -Moved Tilemap_PlatformsNight on the Y axis to realign as it was was no longer on 0. -Note for next time: Readjust 2nd jumping puzzle to fit new alignment.

--- 2D Platformer Example Project/2D Platformer Example Project/Assets/Scripts/DimensionChanger.cs
+++ 2D Platformer Example Project/2D Platformer Example Project/Assets/Scripts/DimensionChanger.cs
@@ -11,12 +11,6 @@ public class DimensionChanger : MonoBehaviour
     public GameObject m_CameraTwo;
     private bool active = true;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    
-    }
-
     // Update is called once per frame
     void Update()
     {
